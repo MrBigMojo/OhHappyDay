dba0a3a9c7873e55ddc343b33793de0af06ff718
Armin Mehinovic <Armin.Mehinovic@virginpulse.com>
2016-12-28 11:35:15 -0500

    Initial Commit

---
+++ OhHappyDay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OhHappyDay
+//
+//  Created by Armin Mehinovic on 12/28/16.
+//  Copyright © 2016 Armin Mehinovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
